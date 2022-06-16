33532ba3eb08f155e2b9185bedb14f69ba60f2f1
Sebastian Hec <frotifratek@gmail.com>
2022-06-16 14:48:04 +0200

    added mixedMessage.js

---
+++ mixedMessage.js
@@ -0,0 +1,6 @@
+let set1 = ['You', 'We', 'They'];
+let set2 = [' are', ' should be', ' will', ' were'];
+let set3 = [' amazing!', ' happy!', ' smiled!', ' brave!'];
+
+let message = set1[Math.floor(Math.random()*3)] + set2[Math.floor(Math.random()*4)] + set3[Math.floor(Math.random()*4)];
+console.log(message);
